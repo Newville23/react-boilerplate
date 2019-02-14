a39035dfdfb2bfd581e0a41b2bea6ca25e272c83
David Villa <david.villanueva.dev@gmail.com>
2019-02-14 18:01:52 -0500

    Add hot loader

--- src/App/index.js
+++ src/App/index.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import styles from './styles.css';
-export default class App extends Component {
+import { hot } from 'react-hot-loader';
+class App extends Component {
   render () {
     return <h1 className={styles.titleRed}>Hello boilerplates </h1>
   }
 }
+
+export default hot(module)(App)
