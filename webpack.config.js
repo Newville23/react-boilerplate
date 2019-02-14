767da6bffc1462c744a0301f753186e415980eac
David Villa <david.villanueva.dev@gmail.com>
2019-02-14 15:27:53 -0500

    Add html template

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,25 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  mode: 'developtment',
   entry: './src/index.js',
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'app.bundle.js'
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: { presets: ['@babel/preset-env'] }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
 }
