b690c24ea9dc382f6aa6ec52d937e413f766248f
David Villa <david.villanueva.dev@gmail.com>
2019-02-19 16:34:58 -0500

    Add jest global config

--- src/App/index.js
+++ src/App/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import styles from './styles.css';
 import { hot } from 'react-hot-loader';
+
 class App extends Component {
   render () {
     return <h1 className={styles.titleRed}>Hello boilerplates </h1>
